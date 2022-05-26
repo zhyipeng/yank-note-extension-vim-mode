d6778564cf9d879032a4f3a51f1e27f757e1e673
zhyipeng <zhyipeng@outlook.com>
2022-05-26 17:35:17 +0800

    feat: 状态栏支持

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,21 @@ registerPlugin({
           'monaco-vim': getExtensionBasePath(__EXTENSION_ID__) + '/dist/monaco-vim.js',
         }
       })
+
+      // monaco-vim 状态栏要求直接使用 dom 操作更新状态
+      let statusBar: HTMLSpanElement
+      const docInfoEls = document.getElementsByClassName('document-info')
+      if (docInfoEls.length > 0) {
+        const docInfo = docInfoEls[0]
+        statusBar = document.createElement('span')
+        statusBar.style.marginRight = '5px'
+        docInfo.parentNode!.insertBefore(statusBar, docInfo)
+      }
+
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       require(['monaco-vim'], function (MonacoVim) {
-        MonacoVim.initVimMode(monaco.editor)
+        MonacoVim.initVimMode(monaco.editor, statusBar)
       })
     })
   }
